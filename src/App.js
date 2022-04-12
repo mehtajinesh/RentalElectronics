1a23f2c563e9c9214d935298df6c834638117e43
Heekyung Kim <heekyung.kim27@gmail.com>
2022-04-12 02:00:13 -0400

    Setup Router for login and register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from './components/LoginScreen/Login'
+import Register from './components/RegisterScreen/Register'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div className="container">
+      <Routes>
+        <Route path="/">
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Route>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
