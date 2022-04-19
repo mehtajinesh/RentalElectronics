4d38ba064763fe46d0c8aded2fdfa6c1f5c3f098
Heekyung Kim <heekyung.kim27@gmail.com>
2022-04-19 00:09:22 -0400

    Delete Context API

--- src/context/loginContext.js
+++
@@ -1,9 +0,0 @@
-import {createContext} from 'react'
-
-const loginContext = createContext({
-    isLoggedin : false,
-    setLoginStatus: () => {}
-    // TODO: add user and auth
-});
-
-export default loginContext
