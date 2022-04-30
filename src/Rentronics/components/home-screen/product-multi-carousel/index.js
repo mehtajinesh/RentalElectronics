14d9837b85e82f32441194ea18db3d44c08262dc
Jinesh Mehta <mehta.jin@northeastern.edu>
2022-04-30 19:47:18 -0400

    changed the profile id to null for now for items

--- src/Rentronics/components/home-screen/product-multi-carousel/index.js
+++ src/Rentronics/components/home-screen/product-multi-carousel/index.js
@@ -22,21 +22,21 @@ const MultiProductCarousel = ({items}) => {
                 <Slider {...settings}>
                     {
                         items.map((itemData) => (
-                                <div className="p-2" key={itemData.item_id}>
+                                <div className="p-2" key={itemData._id}>
 
                                         <div className="card">
-                                            <Link to={`/viewItem/${itemData.item_id}`} state={{ itemData }} className="text-decoration-none">
-                                            <img src={itemData.item_images[0]} className="card-img-top p-4"/>
+                                            <Link to={`/viewItem/${itemData._id}`} state={{ itemData }} className="text-decoration-none">
+                                            <img src={itemData.productImages[0]} className="card-img-top p-4"/>
                                             <div className="card-body">
-                                                <div className="fs-5 card-title text-black">{itemData.item_title}</div>
+                                                <div className="fs-5 card-title text-black">{itemData.productName}</div>
                                             </div>
                                             </Link>
                                             <div className="card-footer">
                                                 <div className="d-flex flex-column justify-content-between">
-                                                    <span className="text-muted my-auto fs-6">Seller: <a href={itemData.item_seller_profile_url}>{itemData.item_seller_name}</a></span>
-                                                    <div className="text-muted my-auto">{`Posted: ${itemData.item_post_date}`}</div>
+                                                    <span className="text-muted my-auto fs-6">Seller: <a href="#">{itemData.item_seller_name}</a></span>
+                                                    <div className="text-muted my-auto">{`Posted: ${itemData.postDate}`}</div>
                                                     <div
-                                                        className="text-muted my-auto fs-6">{`Location: ${itemData.item_location}`}</div>
+                                                        className="text-muted my-auto fs-6">{`Location: ${itemData.location}`}</div>
                                                 </div>
                                             </div>
                                         </div>
